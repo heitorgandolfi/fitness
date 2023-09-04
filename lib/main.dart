483dc9e62e7647e3cb90449857ab2ebd2d60b718
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-09-04 15:59:08 -0300

    feat: added appBar and textField components

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: const HomeScreen(),
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,115 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          _searchField(),
+        ],
+      ),
+    );
+  }
+
+  Container _searchField() {
+    return Container(
+          margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: const Color(0xff1D1617).withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0,
+              )
+            ],
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.all(12),
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+                suffixIcon: const SizedBox(
+                  width: 100,
+                  child: IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        VerticalDivider(
+                          color: Colors.black,
+                          indent: 10,
+                          endIndent: 10,
+                          thickness: 0.1,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(right: 12),
+                          child: Icon(
+                            Icons.tune_rounded,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                )),
+          ),
+        );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      centerTitle: true,
+      elevation: 0,
+      backgroundColor: Colors.white,
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
+      ),
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: const Color(0xFFF7F8F8),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: const Icon(
+            Icons.keyboard_arrow_left_rounded,
+            size: 30,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            padding: const EdgeInsetsDirectional.symmetric(horizontal: 8),
+            margin: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: const Color(0xFFF7F8F8),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: const Icon(
+              Icons.more_horiz_rounded,
+              size: 30,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
